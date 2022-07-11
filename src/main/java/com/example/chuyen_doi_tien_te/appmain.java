374d91ef26607d17e81e523d424cb56599f6d040
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-07-11 11:12:48 +0700

    new

--- src/main/java/com/example/chuyen_doi_tien_te/appmain.java
+++ src/main/java/com/example/chuyen_doi_tien_te/appmain.java
@@ -1,2 +1,26 @@
-package com.example.chuyen_doi_tien_te;public class appmain {
+package com.example.chuyen_doi_tien_te;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api")
+public class appmain {
+    @GetMapping
+    public ResponseEntity tinh(@RequestParam("type") String type, @RequestParam("money") int money){
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        map.put("usd",23000);
+        map.put("eur",23783);
+        map.put("jpy",17163);
+        if(map.containsKey(type)){
+            return ResponseEntity.ok(type+ ": "+map.get(type).intValue()+"=> "+ map.get(type).intValue() * money);
+        }
+        return ResponseEntity.ok("khong tim thay");
+    }
 }
--- src/test/java/com/example/chuyen_doi_tien_te/ChuyenDoiTienTeApplicationTests.java
+++ src/test/java/com/example/chuyen_doi_tien_te/ChuyenDoiTienTeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChuyenDoiTienTeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
